85e08fb21fdb36ffb38df14d79eb80aa4587ea50
Newton Duarte <newton_@hotmail.com>
2023-08-28 21:25:50 -0300

    feat: add clients routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'clients',
+    loadChildren: () => import('./clients/feature/clients-shell/clients-shell.module').then((m) => m.ClientsShellModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
