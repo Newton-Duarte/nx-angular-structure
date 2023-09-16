18edfd3580af1a6921cb81a6fc75e9acda94beaa
Newton Duarte <newton_@hotmail.com>
2023-09-16 05:14:04 -0300

    feat: add material components

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const MaterialComponents = [
   MatButtonModule,
   MatIconModule,
   MatSlideToggleModule,
-  MatListModule
+  MatListModule,
+  MatTooltipModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MatInputModule
 ]
 
 @NgModule({
